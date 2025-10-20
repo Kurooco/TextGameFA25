6ef505abfb52d67d89151b093b63fde805665e15
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-10-20 18:45:22 -0500

    attempted basic visual editor

---
+++ manager.gd
@@ -0,0 +1,7 @@
+extends Control
+
+@onready var text_game_editor = $VBoxContainer/TextGameEditor
+
+
+func _on_play_pressed():
+	pass
---
+++ text_game_editor.gd
@@ -0,0 +1,19 @@
+extends GraphEdit
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_connection_request(from_node, from_port, to_node, to_port):
+	connect_node(from_node, from_port, to_node, to_port)
+
+
+func _on_disconnection_request(from_node, from_port, to_node, to_port):
+	disconnect_node(from_node, from_port, to_node, to_port)
