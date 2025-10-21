80627d3ded768434b41cbf122308c1d598a28cea
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-10-21 11:30:27 -0500

    added connections between rooms

--- main.gd
+++ main.gd
@@ -7,25 +7,22 @@ var current_room = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	current_room = load("res://test_room.tres")
-	print(current_room)
-	#output_box.text = describe_room()
+	output_box.text = describe_room()
 
 
 func describe_room() -> String:
 	var d = "***" + current_room.name + "***\n" + current_room.get_description()
 	d += "\nNearby rooms:\n"
 	for r in current_room.adjacent_rooms:
-		d += r.name + "\n"
+		d += load(r).name + "\n"
 	return d
 	
-func _on_submit_pressed():
-	print(current_room.name)
-	return
-	var input : String = user_input_box.text
+func submit(input: String):
 	var output = ""
 	user_input_box.text = ""
-	if(input.begins_with("go to")):
-		var loc = input.split("go to")[0]
+	if(input.begins_with("go to ")):
+		var loc = input.get_slice("go to ", 1)
+		print("-"+loc+"-")
 		if(current_room.is_room_nearby(loc)):
 			current_room = current_room.get_nearby_room(loc)
 			output = describe_room()
@@ -34,4 +31,12 @@ func _on_submit_pressed():
 	else:
 		output = "I don't know what you mean."
 		
-	output_box.text += "\n" + output + "\n"
+	output_box.text += "\n>>>" + input + "\n" + output + "\n"
+
+
+func _on_submit_pressed():
+	submit(user_input_box.text)
+
+
+func _on_user_input_text_submitted(new_text):
+	submit(new_text)
--- room.gd
+++ room.gd
@@ -2,17 +2,18 @@ extends Describable
 class_name Room
 
 ## WARNING: DEPENDENCY LOOPS. FIX ASAP.
-@export var adjacent_rooms : Array[Room]
+@export var adjacent_rooms : Array[String]
 @export var items : Array[Item]
 
 func is_room_nearby(room:String) -> bool:
 	for r in adjacent_rooms:
-		if(room == r.name):
+		print(room.to_lower() + ", " + load(r).name.to_lower())
+		if(room.to_lower() == load(r).name.to_lower()):
 			return true
 	return false
 
 func get_nearby_room(room:String) -> Room:
 	for r in adjacent_rooms:
-		if(room == r.name):
-			return r
+		if(room.to_lower() == load(r).name.to_lower()):
+			return load(r)
 	return null
